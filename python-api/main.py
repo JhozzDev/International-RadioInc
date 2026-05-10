318cafb916f57c9f8b542ab94392582d44c25cf3
Cristian J. Hiciano <167030296+JhozzDev@users.noreply.github.com>
2026-05-10 13:55:25 -0400

    Fix CORS origin URL case sensitivity

--- python-api/main.py
+++ python-api/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://Radiolanguage.onrender.com"],
+    allow_origins=["http://localhost:3000", "https://radiolanguage.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
