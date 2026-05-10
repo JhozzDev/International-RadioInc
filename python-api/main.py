93dc6a20f0adf33d70c00f28db4ff48593a31948
JhozzDev <jhossanderhiciano@example.com>
2026-05-10 11:53:47 -0400

    Fix cors 2.0

--- python-api/main.py
+++ python-api/main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+    "https://radiolanguages.onrender.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
