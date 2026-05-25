76dbb185f5fe29ea6022b1f1533e2dd361a6cd31
JhozzDev <jhossanderhiciano@example.com>
2026-05-25 19:43:39 -0400

    Culture Info

--- python-api/main.py
+++ python-api/main.py
@@ -6,14 +6,22 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
+<<<<<<< HEAD
     allow_origins=[
         "http://localhost:3000",
     "https://radiolanguage.onrender.com"
     ],
+=======
+    allow_origins=["http://localhost:3000", "https://Radiolanguage.onrender.com"],
+>>>>>>> 1dab861 (Ping)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/ping")
+def ping():
+    return {"Status": "OK"}
 
 @app.get("/radios")
 def GET(country: str):
